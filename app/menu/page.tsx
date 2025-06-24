d9c078fa06845d7a2e43d809a60c11ba4d81b720
gybran nauval <nauvalgybran@gmail.com>
2025-06-24 23:23:53 +0700

    feat(menu): create dedicated menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,12 +1,18 @@
+import Layout from "../components/layout/layout";
+import MenuSection from "../components/sections/menu/MenuSection";
+
 export default function MenuPage() {
   return (
-    <section className="py-12 text-center">
-      <h1 className="text-4xl font-bold mb-4">Menu SEA Catering</h1>
-      <p className="text-gray-600 max-w-2xl mx-auto">
-        Pilih dari berbagai pilihan makanan sehat yang bisa kamu kustom sesuai
-        kebutuhan harianmu. Semua makanan kami dibuat dengan bahan berkualitas
-        dan dikirim langsung ke kota kamu!
-      </p>
-    </section>
+    <Layout>
+      <section className="pt-12 text-center">
+        <h1 className="text-4xl font-bold mb-4">Menu SEA Catering</h1>
+        <p className="text-gray-600 max-w-2xl mx-auto">
+          Pilih dari berbagai pilihan makanan sehat yang bisa kamu kustom sesuai
+          kebutuhan harianmu. Semua makanan kami dibuat dengan bahan berkualitas
+          dan dikirim langsung ke kota kamu!
+        </p>
+      </section>
+      <MenuSection />
+    </Layout>
   );
 }
