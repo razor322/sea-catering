bbc140cb0559215045faad3640c7c0c042b187b3
gybran nauval <nauvalgybran@gmail.com>
2025-06-24 23:08:14 +0700

    feat(ui): integrate global toaster for notifications

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -1,9 +1,9 @@
 // components/Layout.tsx
 
 import { ReactNode } from "react";
-
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -12,6 +12,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
       <main className="flex-grow px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto w-full">
         {children}
+        <Toaster richColors position="top-right" />
       </main>
 
       <Footer />
