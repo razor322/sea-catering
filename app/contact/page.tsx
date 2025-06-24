4b0d7091427284808df74c16e2f5eaf6fc0d82e1
gybran nauval <nauvalgybran@gmail.com>
2025-06-24 23:23:29 +0700

    feat(contact): create dedicated contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/layout";
+import ContactSection from "../components/sections/home/ContactSection";
+
+export default function ContactPage() {
+  return (
+    <Layout>
+      <ContactSection />
+    </Layout>
+  );
+}
