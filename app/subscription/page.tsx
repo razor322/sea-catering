a6529e2947d24be92a0d27f55e561447d8ddd95b
gybran nauval <nauvalgybran@gmail.com>
2025-06-24 23:24:10 +0700

    feat(subscription): create dedicated subscription page

---
+++ app/subscription/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/layout";
+import SubscriptionSection from "../components/sections/subscription/SubscriptionSection";
+
+const SubscriptionPage = () => {
+  return (
+    <div>
+      <Layout>
+        <SubscriptionSection />
+      </Layout>
+    </div>
+  );
+};
+
+export default SubscriptionPage;
