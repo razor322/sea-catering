3b2924307d58985f735c84b2853d96e0b6077efa
gybran nauval <nauvalgybran@gmail.com>
2025-06-28 23:13:48 +0700

    feat(testimonial): implement dynamic data fetching and submission

--- app/components/sections/home/TestimonialsSection.tsx
+++ app/components/sections/home/TestimonialsSection.tsx
@@ -1,10 +1,28 @@
-import TestimonialForm from "../../shared/testimonial/TestimonialForm";
+"use client";
+
 import TestimonialCarousel from "../../shared/testimonial/TestimonialCarousel";
 import { Button } from "@/components/ui/button";
 import Link from "next/dist/client/link";
 import AvatarSection from "../../shared/AvatarSection";
+import { TestimonialForm } from "../../shared/testimonial/TestimonialCard";
+import { Testimonial } from "@prisma/client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function TestimonialsSection() {
+  const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
+
+  // Fetch awal
+  useEffect(() => {
+    axios.get("/api/testimonial").then((res) => {
+      setTestimonials(res.data);
+    });
+  }, []);
+
+  // Tambahkan testimonial baru ke array
+  const addTestimonial = (newItem: Testimonial) => {
+    setTestimonials((prev) => [newItem, ...prev]);
+  };
   return (
     <section className="pt-6 px-4">
       <div className="text-center mb-6">
@@ -13,10 +31,11 @@ export default function TestimonialsSection() {
           Pelanggan kami puas dengan pengalaman makan sehat dari SEA Catering.
         </p>
       </div>
-      <TestimonialCarousel />
+      <TestimonialCarousel testimonials={testimonials} />
       <AvatarSection />
-      <div className="mt-5 border-t ">
-        <TestimonialForm />
+      <div className="my-5  ">
+        {/* <TestimonialForm /> */}
+        <TestimonialForm onSubmitSuccess={addTestimonial} />
       </div>
       <section className="py-12 bg-green-100 text-center rounded-2xl">
         <h2 className="text-2xl font-bold mb-2">Siap Hidup Lebih Sehat?</h2>
